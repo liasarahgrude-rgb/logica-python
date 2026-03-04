769db5c00f79dc9f24be8479e4eff25e04aa1806
Lia Sarah <liasarahgrude@gmail.com>
2026-03-04 20:13:04 -0300

    Delete questao7.py

--- questao7.py
+++
@@ -1,7 +0,0 @@
-##1. Peça a cidade do usuário e permita o acesso apenas se a cidade for “Fortaleza”. 
-# Caso contrário, mostre que o acesso foi negado.
-cidade = input ("Digite a sua cidade: ")
-if cidade == "Fortaleza" or cidade == "fortaleza":
-    print(f"Acesso permitido!")
-else: 
-    print(f"Acesso negado!")
