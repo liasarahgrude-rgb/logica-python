246d60f9aff13edb2e6c312b769de522dd0955ba
Lia Sarah <liasarahgrude@gmail.com>
2026-03-04 20:12:33 -0300

    Delete questao3.py

--- questao3.py
+++
@@ -1,5 +0,0 @@
-##1. Peça a altura do usuário e verifique se é um valor válido (maior que zero). 
-# Caso não seja, informe que a altura é inválida.
-altura = int (input ("Digite sua altura: "))
-if altura < 0:
-    print (f"Sua altura é inválida")
