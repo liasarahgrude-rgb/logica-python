994d8fdf4322a2ddbd6173afb1aad80ceb483427
liasarahgrude-rgb <liasarahgrude@gmail.com>
2026-03-13 19:50:10 -0300

    Adicionando o enunciado dos exercicios

--- aula 04 - Repetição For/questao1.py
+++ aula 04 - Repetição For/questao1.py
@@ -1,4 +1,4 @@
-##Problema "tabuada" Ler um número inteiro N, daí mostrar na tela a tabuada de N para 1 a 10, conforme exemplo. 
+##Problema "tabuada" Ler um número inteiro N, daí mostrar na tela a tabuada de N para 1 a 10.
 num = int (input("Digite um número inteiro: "))
 print(f"Tabuada do {num}:")
 for num2 in range(1, 11):
